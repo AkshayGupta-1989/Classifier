43be90a4fe6baa3a28a53c2031a3f767df05afc4
Akshay Gupta <akshay89gupta@gmail.com>
2018-05-06 12:37:49 -0700

    initial  ommit

---
+++ api/classify.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import SGDClassifier
+from sklearn import metrics
+from sklearn.feature_extraction.text import HashingVectorizer
+from pathlib import Path
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
+
+my_file = Path("model.pkl")
+
+def classify(input):
+    if not my_file.exists():
+
+        # Using SVM as classifier
+        text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', CalibratedClassifierCV(LinearSVC()))])
+
+        # Reading data file (.csv)
+        df = pd.read_csv('shuffled-full-set-hashed.csv', header=None, sep=',', names=['class', 'documents'])
+        df.dropna(inplace=True)
+        X = df['documents']
+        y = df['class']
+
+        # Splitting data into training and validation (67%: training; 33%: validation)
+        X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(X, y, df.index, test_size=0.33, random_state=0)
+
+        # Fit data to the classifier
+        text_clf.fit(X_train,y_train)
+
+        # Saving model in pickel file if the model is not already trained
+        joblib.dump(text_clf, 'model.pkl')
+
+
+    # Reading file to get the model trained
+    text_clf = joblib.load('model.pkl')
+
+    arr = []
+    arr.append(input)
+
+    #predicting the given input
+    y_pred = text_clf.predict(arr)[0]
+
+    # Getting confidence score
+    prob_dist = text_clf.predict_proba(arr)[0]
+    max_prob = (str(prob_dist[text_clf.classes_.tolist().index(y_pred)] * 100)+"%")
+
+
+    # Returning class with its confidence score
+    return y_pred, max_prob
+
---
+++ application.py
@@ -0,0 +1,66 @@
+import random
+from flask import Flask, jsonify, request, render_template
+from flasgger import Swagger
+from api.classify import classify
+
+application = app = Flask(__name__)
+Swagger(application)
+
+@app.route('/classify', methods=['GET'])
+def index():
+    """
+    This is the Document Classification API
+    Call this api passing a document and get back its class
+    ---
+    tags:
+      - Document Classification API
+    parameters:
+     - name: words
+       in: query
+       type: string
+       required: true
+       description: Document words
+    produces: [
+        "application/json",
+        "text/html"
+    ]
+
+    responses:
+      200:
+        description: A class with its confidence
+        schema:
+          id: classification
+          properties:
+            class:
+              type: string
+              description: Document class
+              default: POLICY CHANGE
+            confidence:
+              type: string
+              description: Class confidence
+              default: 74%
+
+    """
+    query = request.args['words']
+    if len(query) == 0:
+        return "An error occurred, enter words", 500
+    else:
+        classification, confidence = classify(query)
+        result = 'class: '+str(classification)+', classification: '+str(confidence)
+
+        accept = (request.headers.get('accept'))
+
+        if (accept == "text/html"):
+            return result
+        else:
+            return jsonify({
+                'class': classification,
+                'confidence': confidence
+            })
+
+
+
+
+
+if __name__ == '__main__':
+    application.run()
